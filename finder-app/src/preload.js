0a1cb85dafc2fd269b1b0a472d9c929bd5e8dfd2
juanlusoft <>
2026-02-18 11:32:31 +0100

    chore: remove old Electron finder-app, replaced by finder-go

--- finder-app/src/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('finder', {
-  scanNetwork: () => ipcRenderer.invoke('scan-network'),
-  openNAS: (url) => ipcRenderer.invoke('open-nas', url)
-});
